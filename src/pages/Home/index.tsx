7001d4c36c57954817be857ad649a85647dac6b2
HarryZardo <zardoharry@gmail.com>
2023-10-23 11:46:14 -0300

    modificando a home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,11 +26,16 @@ export default function Home() {
             <h1 className={styles.informBox__title}> O que é Lixo Eletrônico?</h1>
             <h3 className={styles.informBox__subtitle}>
               Lixo eletrônico, também conhecido como "e-lixo" ou "resíduos eletrônicos", refere-se a equipamentos eletrônicos descartados, danificados ou obsoletos, que incluem dispositivos como computadores, smartphones, tablets, televisores, aparelhos domésticos e outros componentes eletrônicos.
+              <br></br>
+              <br></br>
               Esses produtos contêm substâncias tóxicas, como chumbo, mercúrio e cádmio, que podem ser prejudiciais ao meio ambiente e à saúde humana se não forem descartados adequadamente.
+              <br></br>
+              <br></br>
               O descarte inadequado de lixo eletrônico é um problema crescente, levando a poluição ambiental e a perda de recursos valiosos.
               Portanto, a reciclagem e a gestão responsável de lixo eletrônico são essenciais para mitigar esses impactos negativos.
             </h3>
-            <br></br>
+
+
             <h1 className={styles.informBox__title}> Processo do Lixo Eletrônico</h1>
             <h3 className={styles.informBox__subtitle}>
               <li>
@@ -62,6 +67,48 @@ export default function Home() {
                 Destinação final: Os resíduos finais são encaminhados para aterros sanitários apropriados ou incinerados de forma controlada, quando não podem ser reciclados.
               </li>
             </h3>
+
+
+            <h1 className={styles.informBox__title}>A falta de coleta do lixo eletrônico.</h1>
+            <h3 className={styles.informBox__subtitle}>
+            No Brasil há uma lei que determina que os fabricantes são os responsáveis por receber de volta os produtos eletrônicos que venderam e que serão descartados. Esse processo é chamado de logística reversa.
+            <br></br>
+            <br></br>
+            Contudo, a logística reversa ainda é um grande desafio para o Brasil. Em primeiro lugar, porque os custos do processo de coleta, separação e destinação do lixo às empresas de reciclagem acabam sendo empurrados do fabricante para o vendedor e consequentemente para o consumidor final, quando na realidade essa deveria ser uma responsabilidade compartilhada entre todos os atores da PNRS.
+            <br></br>
+            <br></br>
+            Em segundo lugar, porque ainda há a falta de esclarecimento para que as pessoas façam o descarte de forma responsável. E isso envolve a compreensão da separação correta de materiais até o conhecimento de postos de coleta para a entrega do lixo reciclável. E assim, milhões de toneladas de lixo continuam sendo descartadas de forma incorreta no país.
+            <br></br>
+            <br></br>
+            </h3>
+
+
+            <h1 className={styles.informBox__title}>Como descartar eletrodomésticos?</h1>
+            <h3 className={styles.informBox__subtitle}>
+            Você já entendeu que é importante descartar eletrodomésticos da forma correta, mas como seria isso? Primeiramente, deve-se salientar que o descarte desse tipo de lixo deve ser feito de forma organizada.
+            <br></br>
+            <br></br>
+            Sendo assim, se houver partes quebradas e pontiagudas, por exemplo, vale embrulhar esses cacos e sinalizar para quem ninguém se machuque. Além disso, se ainda possui a caixa do produto, é ideal que se desfaça desse lixo também.
+            <br></br>
+            <br></br>
+            Portanto, guarde as partes quebradas a serem descartadas na caixa e procure por um local de coleta. Esse é o primeiro passo, mas ainda é necessário dar destino ao restante do material, sempre de forma segura e sem prejudicar o meio ambiente.
+            <br></br>
+            <br></br>
+            Como os eletrodomésticos precisam de alguns cuidados específicos, algumas fazem coletas para residências e também empresariais em São Paulo. O lado positivo é que elas são autorizadas para o serviço e você pode conferir a lista delas no site da prefeitura de sua cidade.
+            <br></br>
+            <br></br>
+            Imagina que fazer isso sem ajuda profissional exige no mínimo que você tenha um veículo que possa carregar o seu eletrodoméstico. Depois, precisa saber onde realizar o descarte, já que esse tipo de resíduo não pode ser colocado em latas de lixo ou semelhantes.
+            <br></br>
+            <br></br>
+            Em muitos casos os condomínios já oferecem parcerias para que os moradores possam ter o descarte de eletrodomésticos facilitados. Assim, as empresas vão até o local e recolhe o resíduo, economizando tempo e dinheiro.
+            <br></br>
+            <br></br>
+            Entretanto, é claro que não é qualquer empresa que pode fazer isso. Ao acessar a lista das empresas autorizadas na sua cidade, verifique também o trabalho delas e procure recomendações positivas.
+            <br></br>
+            <br></br>
+            </h3>
+
+
           </div>
         </div>
       </div>
