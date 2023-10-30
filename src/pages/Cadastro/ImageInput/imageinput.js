83b53433088b29d626549ada07eec47c56e94dd3
HarryZardo <zardoharry@gmail.com>
2023-10-30 11:38:49 -0300

    arrumando caminhos e a ellipse

--- src/pages/Cadastro/ImageInput/imageinput.js
+++ src/pages/Cadastro/ImageInput/imageinput.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import endImg from "../../../assets/cadastro-login/icone_usuario.png";
+import endImg from "../../../assets/components_img/icone_usuario.png";
 import styles from "./ImageInput.module.scss"
 import { IoIosAdd } from "react-icons/io";
 
 function App() {
 
-  const [image, setImage] = useState('');  
+  const [image, setImage] = useState('');
 
   const uploadImage = async e => {
     e.preventDefault();
@@ -22,10 +22,10 @@ function App() {
           {image ? <img src={URL.createObjectURL(image)} alt="Imagem" className={styles.imageActive} /> : <img src={endImg} alt="Imagem" className={styles.imageNotActive} />}
         </label>
 
-          <label htmlFor="image-file" className={styles.button}> 
+          <label htmlFor="image-file" className={styles.button}>
             <IoIosAdd size={"25px"}/>
           </label>
-        
+
       </div>
         <input type="file" name="image-file" id="image-file" onChange={e => setImage(e.target.files[0])} className={styles.imageInput}/>
       </form>
