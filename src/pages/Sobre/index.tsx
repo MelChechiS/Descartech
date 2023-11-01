99c7e4e59de80d577597b6cce47a23b4bb90bf06
HarryZardo <zardoharry@gmail.com>
2023-11-01 08:26:01 -0300

    files

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,6 +1,8 @@
 import Toolbar from "../../components/Toolbar/index";
 import styles from "../Sobre/Sobre.module.scss";
 import Footer from "../../components/Footer";
+import Harry from "../../assets/desenvolvedores/harrytcc.jpg.webp";
+import Mel from "../../assets/desenvolvedores/mel.webp"
 
 export default function Inicial() {
   return (
@@ -11,6 +13,7 @@ export default function Inicial() {
       <div className={styles.sobre}>
         <h1 className={styles.sobre__h1}>Desenvolvedores do projeto.</h1>
         <h3 className={styles.sobre__h3}>Harry Zardo</h3>
+        <img src={Mel} className={styles.sobre__img}></img>
 
       </div>
 
