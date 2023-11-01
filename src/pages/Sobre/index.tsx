cef1a939f0d4da46336b71f615d7ffad68f4ea5b
HarryZardo <zardoharry@gmail.com>
2023-11-01 08:56:29 -0300

    criando sobre

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,7 +1,7 @@
 import Toolbar from "../../components/Toolbar/index";
 import styles from "../Sobre/Sobre.module.scss";
 import Footer from "../../components/Footer";
-import Harry from "../../assets/desenvolvedores/harrytcc.jpg.webp";
+import Harry from "../../assets/desenvolvedores/harrytcc.webp";
 import Mel from "../../assets/desenvolvedores/mel.webp"
 
 export default function Inicial() {
@@ -10,10 +10,21 @@ export default function Inicial() {
       <header>
         <Toolbar />
       </header>
+      <div className={styles.projeto}>
+        <h1 className={styles.projeto__h1}>Sobre o projeto da Descartech</h1>
+        <p className={styles.projeto__p}>
+          Esse site é nosso projeto de Trabalho de Conclusão do Curso, desenvolvido durante o curso de Desenvolvimento de Sistemas na ETEC "Cidade do Livro".
+          <br></br>
+          Para o desenvolvimento do trabalho, foram utilizados componentes e habilidades adquiridas durante as aulas nesses três anos de formação.
+        </p>
+
+      </div>
+      <div className={styles.desenvolvedores}>
+        <h1 className={styles.desenvolvedores__h1}>Desenvolvedores do projeto.</h1>
+      </div>
       <div className={styles.sobre}>
-        <h1 className={styles.sobre__h1}>Desenvolvedores do projeto.</h1>
         <h3 className={styles.sobre__h3}>Harry Zardo</h3>
-        <img src={Mel} className={styles.sobre__img}></img>
+        <img src={Harry} className={styles.sobre__img}></img>
 
       </div>
 
